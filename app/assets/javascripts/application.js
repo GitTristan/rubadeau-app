339fb09c702dba1b9bd765d6eb85b6a9c0e0d421
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-09 18:34:08 -0600

    new slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,53 +16,9 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
-$(document).ready(function() {
-  setInterval(rotateImages, 4000);
+$(document).ready(function(){
+     $("#myCarousel").carousel({
+         interval : 5000,
+         pause: false
+     });
 });
-
-function rotateImages(){
-  $("#photoShow").animate({marginLeft: "-400px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-800px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-1200px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-1600px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-2000px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-2400px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-2800px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-3200px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-3600px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-4000px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-4400px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-4800px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-5200px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-5600px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-6000px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-6400px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-6800px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-7200px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-7600px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-8000px"}, 1000).delay(4000);
-
-  $("#photoShow").animate({marginLeft: "-8400px"}, 1000).delay(4000);
-
-
-  $("#photoShow").animate({marginLeft: "-0px"}, 1000).delay(4000);
-}
